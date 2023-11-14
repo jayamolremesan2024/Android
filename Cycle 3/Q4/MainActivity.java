96385388783346253042da6b2a3c79fd77ebe30d
JayamolRemesh <125429451+jayamolremesan2024@users.noreply.github.com>
2023-11-14 09:54:54 +0530

    Add files via upload

--- Cycle 3/Q4/MainActivity.java
+++ Cycle 3/Q4/MainActivity.java
@@ -1,41 +1,34 @@
-package com.example.sjcet.menuapplication;
+package com.example.sjcet.c3q5;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.MenuInflater;
-import android.view.Menu;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Toast;
-import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+    ListView l;
+    String tutorials[]
+            = { "Algorithms", "Data Structures",
+            "Languages","dbms","java","python"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu_example, menu);
-        return true;
-    }
+        l = findViewById(R.id.list);
+        final ArrayAdapter<String> arr;
+        arr = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, tutorials);
+        l.setAdapter(arr);
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.hello:
-                Toast.makeText(this, "Hello", Toast.LENGTH_SHORT).show();
-                return true;
-            case R.id.logout:
-                Toast.makeText(this, "Logout", Toast.LENGTH_SHORT).show();
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
-        }
-    }
-    public void logout()
-    {
-        Toast.makeText(this,"bye",Toast.LENGTH_SHORT).show();
+        l.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                String value = arr.getItem(position);
+                Toast.makeText(getApplicationContext(), value, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
